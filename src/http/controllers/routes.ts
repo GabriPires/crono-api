7112d3e5b0337e1f5c40565cc21b192f0e155ae9
GabriPires <gabpiresdev@gmail.com>
2024-02-12 12:45:23 -0300

    ✨ feat(register): register controller to handle with requests

---
+++ src/http/controllers/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+
+import { usersRoutes } from './users/routes'
+
+export async function routes(app: FastifyInstance) {
+  app.register(usersRoutes)
+}
---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+
+import { register } from './register.controller'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/register', register)
+}
