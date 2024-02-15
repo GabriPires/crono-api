2d83c8e943820ee8c5298366ed5212369caf199a
GabriPires <gabpiresdev@gmail.com>
2024-02-15 16:42:20 -0300

    ✨ feat(logout): logout controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 import { authenticate } from './authenticate.controller'
+import { logout } from './logout.controller'
 import { profile } from './me.controller'
 import { refresh } from './refresh.controller'
 import { register } from './register.controller'
@@ -11,5 +12,6 @@ export async function usersRoutes(app: FastifyInstance) {
   app.get('/me', { onRequest: [verifyJwt] }, profile)
   app.post('/register', register)
   app.post('/authenticate', authenticate)
+  app.post('/logout', logout)
   app.post('/refresh', refresh)
 }
