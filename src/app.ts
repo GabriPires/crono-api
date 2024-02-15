4c2c51be9f089b0c6c91c4ababeb5d481c034b05
GabriPires <gabpiresdev@gmail.com>
2024-02-15 11:40:06 -0300

    ✨ feat(authenticate): auth and refresh token controllers and some more details
    error handler, jwt middleware, jwt type declaration

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,39 @@
+import cookies from '@fastify/cookie'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
+import { env } from './env'
 import { routes } from './http/controllers/routes'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '15m',
+  },
+})
+app.register(cookies)
+
 app.register(routes)
+
+app.setErrorHandler((error, _, response) => {
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Log error on external service
+  }
+
+  return response.status(500).send({ message: 'Internal server error.' })
+})
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticate } from './authenticate.controller'
+import { refresh } from './refresh.controller'
 import { register } from './register.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/register', register)
+  app.post('/authenticate', authenticate)
+  app.post('/refresh', refresh)
 }
